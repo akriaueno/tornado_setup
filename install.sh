a55aba20691957089a02fa4a3488591a20230adc
Akira Ueno <akiraueno@outlook.com>
2018-03-08 01:18:23 +0900

    fix: form .py to .sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p $HOME/.scripts/setup/tornado
 cp tornado_setup.sh template.py template.html template.css $HOME/.scripts/setup/tornado
-echo "alias tornado_setup='$HOME/.scripts/setup/tornado/tornado_setup.py'" >> $HOME/.bashrc
+echo "alias tornado_setup='$HOME/.scripts/setup/tornado/tornado_setup.sh'" >> $HOME/.bashrc
 source $HOME/.bashrc
