d439517fc42426c0cd5ce9c096d0fc079239a004
Akira Ueno <akiraueno@outlook.com>
2018-03-08 01:27:07 +0900

    fix: add source comand

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p $HOME/.scripts/setup/tornado
 cp tornado_setup.sh template.py template.html template.css $HOME/.scripts/setup/tornado
-echo "alias tornado_setup='$HOME/.scripts/setup/tornado/tornado_setup.sh'" >> $HOME/.bashrc
+echo "alias tornado_setup='source $HOME/.scripts/setup/tornado/tornado_setup.sh'" >> $HOME/.bashrc
 source $HOME/.bashrc
