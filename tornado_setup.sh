b2e585598f72fb7652484401c20804dcba2b9d9f
Akira Ueno <akiraueno@outlook.com>
2018-03-08 00:50:23 +0900

    add: Add setup script

--- tornado_setup.sh
+++ tornado_setup.sh
@@ -5,8 +5,8 @@ cd $1
 /opt/python/versions/3.6.4/bin/python3 -m venv venv
 source ./venv/bin/activate
 pip install tornado
-cp $HOME/.scripts/templates/tornado/template.py ./server.py
+cp $HOME/.scripts/setup/tornado/template.py ./server.py
 mkdir -p templates static/css
-cp $HOME/.scripts/templates/tornado/template.html ./templates/index.html
-cp $HOME/.scripts/templates/tornado/template.css ./static/css/style.css
+cp $HOME/.scripts/setup/tornado/template.html ./templates/index.html
+cp $HOME/.scripts/setup/tornado/template.css ./static/css/style.css
 
