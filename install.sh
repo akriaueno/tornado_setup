a32c2785d13e14fdf25eb97cf5c363998e542bb9
Akira Ueno <akiraueno@outlook.com>
2018-03-08 21:58:34 +0900

    change: Change name and installation directory

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p $HOME/.scripts/setup/tornado
-cp tornado_setup.sh template.py template.html template.css $HOME/.scripts/setup/tornado
-echo "alias tornado_setup='source $HOME/.scripts/setup/tornado/tornado_setup.sh'" >> $HOME/.bashrc
-source $HOME/.bashrc
